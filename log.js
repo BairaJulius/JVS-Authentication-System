76b366b2e699081e56d0097dbde7d3f535f53f0a
BairaJulius <149606089+BairaJulius@users.noreply.github.com>
2024-02-07 15:28:40 +0300

    Add files via upload

---
+++ log.js
@@ -0,0 +1,13 @@
+//LOGIN INTO THE SYSTEM 
+function userValidifaction(email, password){
+
+    const userEmail = "2200704347@mubs.ac.ug"
+    const userPassword = "2200704347@2024"
+
+    if (email === userEmail & password === userPassword){
+        console.log ("USE");
+    } else { console.log("INVALID CREDENTIALS");
+
+    }}
+
+userValidifaction("2200704347@mubs.ac.ug","2200704347@2024");
